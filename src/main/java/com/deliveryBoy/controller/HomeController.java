8e397641b5b6ed0a1daec3f22e5987b1e3352ad9
KajalSet <kajals@solwyz.com>
2025-01-23 10:19:43 +0530

    new commit

--- src/main/java/com/deliveryBoy/controller/HomeController.java
+++ src/main/java/com/deliveryBoy/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,7 +20,7 @@ import com.deliveryBoy.request.HomeRequest;
 import com.deliveryBoy.service.HomeService;
 
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/home")
 public class HomeController {
